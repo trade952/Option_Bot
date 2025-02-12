c3689b72d843a5a949fea96f1ced328c697b1b75
johnn <johnntrade952@gmail.com>
2025-02-12 17:24:46 +0200

    Added real trading logic and enhanced Web Interface

--- Option_Bot/index.js
+++ Option_Bot/index.js
@@ -22,8 +22,10 @@ app.listen(PORT, () => console.log(`📡 Το Web Interface τρέχει στη 
   console.log('🚀 Ξεκινάει το Puppeteer...');
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: '/usr/bin/google-chrome',
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
+  
 
   const page = await browser.newPage();
   console.log('✅ Το Puppeteer ξεκίνησε σωστά με το ενσωματωμένο Chromium!');
